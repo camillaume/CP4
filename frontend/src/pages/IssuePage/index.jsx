d42820d140bb32481fa24fa3ceb9ab41845c7b19
camille <camille.arvieu@gmail.com>
2022-07-21 17:01:44 +0200

    ressources file

---
+++ frontend/src/pages/IssuePage/index.jsx
@@ -0,0 +1,10 @@
+import IssueCard from "@components/IssueCard";
+import SPageIssue from "./style";
+
+export default function IssuePage() {
+  return (
+    <SPageIssue>
+      <IssueCard />
+    </SPageIssue>
+  );
+}
